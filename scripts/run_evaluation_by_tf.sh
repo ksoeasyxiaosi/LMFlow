580e286dc7471b6222f80dff90a95658cadadfab
ksoeasyxiaosi <ksoeasyxiaosi@sina.cn>
2023-04-18 17:05:58 +0800

    hotfix

--- scripts/run_evaluation_by_tf.sh
+++ scripts/run_evaluation_by_tf.sh
@@ -3,7 +3,7 @@
 CUDA_VISIBLE_DEVICES=0 \
     deepspeed examples/evaluate.py \
     --answer_type medmcqa \
-    --model_name_or_path output_models/llama30b-lora-170k \
+    --model_name_or_path llama30b-lora-170k \
     --dataset_path data/MedQA-USMLE/validation \
     --deepspeed examples/ds_config.json \
     --metric accuracy
